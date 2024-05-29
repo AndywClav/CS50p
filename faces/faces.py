7e3fcc4ae26b61d64f2f9aebefdcbd1dbe7c66f9
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-05-29 12:25:17 +0000

    Wed, May 29, 2024, 9:25 AM -03:00

--- faces/faces.py
+++ faces/faces.py
@@ -3,7 +3,7 @@ def convert (str):
     #happy = '\U0001F642'
     #sad = '\U0001F641' # Buscar la forma de colocar caras sin condicionales
     str = input()
-    trasla = chr(ord(str))
+    trasla = ord(str)
     print(trasla)
 
 convert(':()')
