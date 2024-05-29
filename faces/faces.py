8734c96f3468ef4640b3a4a4ca4f2538b231bc91
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-05-29 13:02:25 +0000

    Wed, May 29, 2024, 10:02 AM -03:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,6 @@
-
 def convert (emoji):
     happy = '\U0001F642'
-    sad = '\U0001F641' # Buscar la forma de colocar caras sin condicionales
+    sad = '\U0001F641'
     emoji = emoji.replace(":)", chr(ord(happy)))
     emoji = emoji.replace(":(", chr(ord(sad)))
     return emoji
