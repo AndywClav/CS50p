a7de2600b553783b97a093b2c0928b8f74424575
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-05-30 12:30:09 +0000

    Thu, May 30, 2024, 9:30 AM -03:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,7 +1,7 @@
 def main():
     Mass = float(input(': '))
     SpeedLight = 300000000
-    Energy = Mass * SpeedLight**2
+    Energy = Mass * pow(SpeedLight, 2)
     print(f'{Energy} is equal that E=mc²')
 
 main()
