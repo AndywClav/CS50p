4c2cb3fe2deef3304bbdac28462c6d8e4127be29
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-05-31 12:42:29 +0000

    Fri, May 31, 2024, 9:42 AM -03:00

--- tip/tip.py
+++ tip/tip.py
@@ -6,14 +6,14 @@ def main():
 
 
 def dollars_to_float(d):
-    d.replace('$', '')
+    d = d.replace('$', '')
     dollars = float(d)
     dollars = round(dollars, 2)
     return dollars
 
 
 def percent_to_float(p):
-    p.replace('%', '')
+    p = p.replace('%', '')
     percent = float(p) / 100
     return percent
 
