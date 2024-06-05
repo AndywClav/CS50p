130f215e837ade42403b29ef02a9f0860bba763a
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-05 12:28:20 +0000

    Wed, Jun 5, 2024, 9:28 AM -03:00

--- set1/bank/bank.py
+++ set1/bank/bank.py
@@ -1,8 +1,8 @@
 ask = input("Greeting: ").lower()
 zero = ["hello"]
-twenty = ["h"]
+
 if ask == zero[0]:
     print('$', 0)
 
-elif ask == twenty: # Look validation 
-    print('$', 20)
+ask in 'h'
+
