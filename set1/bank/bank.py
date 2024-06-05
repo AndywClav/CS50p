5d84f3af12a5bdf1ae6ed88a0575b112ac3e3176
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-05 12:48:31 +0000

    Wed, Jun 5, 2024, 9:48 AM -03:00

--- set1/bank/bank.py
+++ set1/bank/bank.py
@@ -2,7 +2,7 @@ ask = input("Greeting: ").lower()
 zero = ["hello"]
 twenty  = 'h' in ask
 
-if ask == zero[0]:
+if ask.replace(' ', '') == zero[0]:
     print('$0')
 elif twenty == True:
     print('$20')
