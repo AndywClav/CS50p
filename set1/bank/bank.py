dfd5f1f2606d92784d59be81a88965b3a8e1607f
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-05 12:57:48 +0000

    Wed, Jun 5, 2024, 9:57 AM -03:00

--- set1/bank/bank.py
+++ set1/bank/bank.py
@@ -1,8 +1,13 @@
 ask = input("Greeting: ").lower()
 findHello  = 'hello' in ask
 findH  = 'h' in ask
+findQ  = '?' in ask
 
 if findHello:
     print('$0')
 elif findH:
     print('$20')
+elif findQ:
+    print('$100')
+else:
+    print('This is outher alternative')
