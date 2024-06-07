3e134e504fe7546ee9233293ae4bdff0be40b6df
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-07 12:40:15 +0000

    Fri, Jun 7, 2024, 9:40 AM -03:00

--- set1/extensions/extensions.py
+++ set1/extensions/extensions.py
@@ -1,7 +1,9 @@
 filleIn = input("Fila ame: ").lower()
 FilleOut = filleIn.replace(".", "/").strip()
 if 'gif' in FilleOut or 'jpg' in FilleOut or 'jpeg' in FilleOut or 'png' in FilleOut:
-    print()
+    print('img/')
+else:
+    print('')
 
 
 # the name the fille need change for image depend the format
