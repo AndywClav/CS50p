5d909fe1234c91c1c0949e14ecb4a948eea63440
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-07 13:09:23 +0000

    Fri, Jun 7, 2024, 10:09 AM -03:00

--- set1/extensions/extensions.py
+++ set1/extensions/extensions.py
@@ -4,14 +4,13 @@ part = filleOut.split('/')
 exten = part[1]
 
 if len(part) >= 2:
-    if 'gif' in filleOut or 'jpeg' in filleOut or 'png' in filleOut:
-        print('image/' + exten)
-    else:
-        match exten:
-            case 'jpg':
-                print('image/jpeg')
-            case _:
-                print('application/octet-stream')
+    match exten:
+        case 'gif' or 'jpeg' or 'png':
+            print('image/' + exten)
+        case 'jpg':
+            print('image/jpeg')
+        case _:
+            print('application/octet-stream')
 
 
 # the name the fille need change for image depend the format
