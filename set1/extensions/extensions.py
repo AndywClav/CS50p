0c9d408f15e6c0fdb72b7265487f5993354d7264
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-07 12:54:19 +0000

    Fri, Jun 7, 2024, 9:54 AM -03:00

--- set1/extensions/extensions.py
+++ set1/extensions/extensions.py
@@ -5,6 +5,15 @@ subtipo = FilleOut[0]
 
 print(subtipo)
 
+filleIn = input("Fila name: ").lower().strip()
+filleOut = filleIn.replace(".", "/")
+partes = filleOut.split('/')
+
+# Obtener la primera parte
+subtipo = partes[0]
+
+print("El tipo ingresado es:", subtipo)
+
 # # Verificar que se hayan ingresado exactamente dos partes
 # if len(FilleOut) > 2:
 #     subtipo = FilleOut[1]
