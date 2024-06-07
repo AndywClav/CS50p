712ee8955f9db7ee033b33f50706ac15360c56bd
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-07 12:38:02 +0000

    Fri, Jun 7, 2024, 9:38 AM -03:00

--- set1/extensions/extensions.py
+++ set1/extensions/extensions.py
@@ -1,4 +1,7 @@
-filleIn = input("Fila ame: ")
+filleIn = input("Fila ame: ").lower()
 FilleOut = filleIn.replace(".", "/").strip()
-if in FilleOut
-print() # the name the fille need change for image depend the format
+if 'gif' in FilleOut or 'jpg' in FilleOut or 'jpeg' in FilleOut or 'png' in FilleOut:
+    print()
+
+
+# the name the fille need change for image depend the format
