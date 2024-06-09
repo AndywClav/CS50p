994283a823dbbec6886fcabfe105cda4013eb0fc
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-09 23:23:22 +0000

    Sun, Jun 9, 2024, 8:23 PM -03:00

--- set1/interpreter/interpreter.py
+++ set1/interpreter/interpreter.py
@@ -6,6 +6,9 @@ multi = '*' in calculator
 divis = '/' in calculator
 
 if sum == True:
+    sepa = sum.split('+')
+    part = sepa
+    total = int(part[0]) + int(part[1])
     print(calculator)
 elif sub == True:
     print(calculator)
