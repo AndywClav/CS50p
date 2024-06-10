a00df2ccb4bb500d0e5dc12b44a1132aef8c89d7
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-10 12:48:45 +0000

    Mon, Jun 10, 2024, 9:48 AM -03:00

--- set1/meal/meal.py
+++ set1/meal/meal.py
@@ -8,15 +8,22 @@ def main():
 
 def convert(time):
     hours, minutes = time.split(":")
-
-    if float(hours) >= 7 and float(hours) <= 9:
-        print('breakfast time')
-    elif int(hours) >= 12 and int(hours) <= 13:
-        print('lunch time')
-    elif int(hours) >= 18 and int(hours) <= 19:
-        print('dinner time')
+    point = time.split(".")
+    if float(point) == '.':
+        print(point)
     else:
-        print('')
+        if int(hours) >= 7 and int(hours) <= 9:
+                print('breakfast time')
+        else:
+                print('')
+
+
+    # elif int(hours) >= 12 and int(hours) <= 13:
+    #     print('lunch time')
+    # elif int(hours) >= 18 and int(hours) <= 19:
+    #     print('dinner time')
+    # else:
+    #     print('')
 
 
 if __name__ == "__main__":
