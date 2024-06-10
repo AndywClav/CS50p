2e30b79d71001c4b2f07dc3f89285a08b23ede0b
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-10 12:43:45 +0000

    Mon, Jun 10, 2024, 9:43 AM -03:00

--- set1/meal/meal.py
+++ set1/meal/meal.py
@@ -9,7 +9,7 @@ def main():
 def convert(time):
     hours, minutes = time.split(":")
 
-    if int(hours) >= 7 and int(hours) <= 9:
+    if float(hours) >= 7 and float(hours) <= 9:
         print('breakfast time')
     elif int(hours) >= 12 and int(hours) <= 13:
         print('lunch time')
