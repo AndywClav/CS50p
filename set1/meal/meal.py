95dbbe5ab0e390ca74bf06fea9182e9ba0b0d386
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-10 12:11:47 +0000

    Mon, Jun 10, 2024, 9:11 AM -03:00

--- set1/meal/meal.py
+++ set1/meal/meal.py
@@ -1,9 +1,10 @@
 def main():
-    ...
+    time = input("What time is it? ").strip()
+    convert(time)
 
 
 def convert(time):
-    ...
+    hours, minutes = time.split(":")
 
 
 if __name__ == "__main__":
