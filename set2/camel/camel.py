d570b7402f8526b6e3700892fdf63f7d009362c0
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-16 23:01:18 +0000

    Sun, Jun 16, 2024, 8:01 PM -03:00

--- set2/camel/camel.py
+++ set2/camel/camel.py
@@ -7,7 +7,7 @@ for char in camelCase:
     else:
         snake_case += char
 
-# Asegurarse de que la cadena no comience con un guion bajo
+        
 if snake_case.startswith('_'):
     snake_case = snake_case[1:]
 
