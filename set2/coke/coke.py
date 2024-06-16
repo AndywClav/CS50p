f341ecbb6a6f8d0f83f440ac6adea28cb4a7cda3
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-16 23:33:21 +0000

    Sun, Jun 16, 2024, 8:33 PM -03:00

--- set2/coke/coke.py
+++ set2/coke/coke.py
@@ -1,8 +1,9 @@
 coca_cola = 50
+print(f'Amount Due: { coca_cola }')
 posible_value = [25, 10, 5]
 
 while coca_cola > 0:
-    print(f'Amount Due: { coca_cola }')
     insert = int(input('Insert Coin: '))
     if insert in posible_value:
         change_own = coca_cola - insert
+    print(f'Amount Due: { change_own }')
