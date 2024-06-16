40097ddc2d71c4b7d5f5185e2abce7e6e53dd8ae
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-16 22:46:25 +0000

    Sun, Jun 16, 2024, 7:46 PM -03:00

--- set2/camel/camel.py
+++ set2/camel/camel.py
@@ -2,7 +2,6 @@ camelCase = input('camelCase: ').strip()
 out = camelCase.title() in camelCase
 
 if out == out:
-    print(f'snake_case: {camelCase}')
-elif out != out:
+    
     print(f'snake_case: {camelCase}')
 
