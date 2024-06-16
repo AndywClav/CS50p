cb1e5a6a94d32c80e6d3cd94a3f98c85c80f15fc
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-16 22:41:08 +0000

    Sun, Jun 16, 2024, 7:41 PM -03:00

--- set2/camel/camel.py
+++ set2/camel/camel.py
@@ -1,9 +1,7 @@
 camelCase = input('camelCase: ').strip()
 for i in camelCase:
-    print(i)
-
-# if i == i.upper():
-#     print(f'snake_case: {camelCase}')
-# elif: i == i:
-#     print(f'snake_case: {camelCase}')
+    if i == i.upper():
+        print(f'snake_case: {camelCase}')
+    elif i == i:
+        print(f'snake_case: {camelCase}')
 
