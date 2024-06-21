f9bb3c0eaacfba5df7ef433b5c2bfa4fdf9dadb0
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-21 18:10:12 +0000

    Fri, Jun 21, 2024, 3:10 PM -03:00

--- set2/twttr/twttr.py
+++ set2/twttr/twttr.py
@@ -1,11 +1,9 @@
 twttr = input("Input: ").lower().strip()
-vowels = ['a', 'e', 'i', 'o', 'u',]
+vowels = ['a', 'e', 'i', 'o', 'u']
 out = ""
 
 for char in twttr:
-    if char in vowels:
-        continue
-    else:
+    if char not in vowels:
         out += char
 
 
