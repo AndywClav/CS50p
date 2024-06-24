12339973d9aad01422c01bfbdfeec962668877a1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-24 12:30:48 +0000

    Mon, Jun 24, 2024, 9:30 AM -03:00

--- set2/plates/plates.py
+++ set2/plates/plates.py
@@ -5,12 +5,23 @@ def main():
     else:
         print("Invalid")
 
-
 def is_valid(s):
-    if s.isupper():
-        if len(s) >= 2 and len(s) <= 6:
-            if '.' not in s and s[2] != '0':
-                return True
+    if len(s) < 2 or len(s) > 6:
+        return False
 
+    if not s[:2].isalpha():
+        return False
+
+    if not s.isalnum():
+        return False
+
+    found_number = False
+    for i in range(len(s)):
+        if s[i].isdigit():
+            found_number = True
+            if not s[i:].isdigit() or (i > 0 and s[i] == '0' and not s[i - 1].isalpha()):
+                return False
+
+    return True
 
 main()
