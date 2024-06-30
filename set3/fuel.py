e9d129aabbdf8eedb46f2d935f6623fa1962ff27
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-30 01:49:14 +0000

    Sat, Jun 29, 2024, 10:49 PM -03:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -1,12 +1,15 @@
-while True:
-    try:
-        fraction = input("Fraction: ").strip()
-        x, y = fraction.split("/")
-        x = int(x)
-        y = int(y)
-        resul = x / y
-        return resul
-    except ValueError:
-        print(f'ValueError')
-    except ZeroDivisionError:
-        print(f'ZeroDivisionError')
+def validation():
+    while True:
+        try:
+            fraction = input("Fraction: ").strip()
+            x, y = fraction.split("/")
+            x = int(x)
+            y = int(y)
+            resul = x / y
+            return resul
+        except ValueError:
+            print(f'ValueError')
+        except ZeroDivisionError:
+            print(f'ZeroDivisionError')
+
+validation()
