f1a1deb7c5c31a804f1b51fac1d49d7e1f5215bc
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-30 01:45:32 +0000

    Sat, Jun 29, 2024, 10:45 PM -03:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -4,7 +4,9 @@ while True:
         x, y = fraction.split("/")
         x = int(x)
         y = int(y)
-        break
+        resul = x / y
+        return result
+        breal
     except ValueError:
         print(f'ValueError')
     except ZeroDivisionError:
