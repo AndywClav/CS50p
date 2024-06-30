cec9b99656018f0ca424d30856e6944e7236d6ff
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-06-30 01:29:59 +0000

    Sat, Jun 29, 2024, 10:29 PM -03:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -2,7 +2,7 @@ while True:
     try:
         fraction = int(input("Fraction: ")).strip()
         x, y = fraction.split("/")
-        if x == int and y == int
+        if x == int and y == int:
             print(f'{x}:{y}')
     except ValueError:
         print(f'ValueError')
