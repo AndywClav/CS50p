233cecdae1143faf30d797ee88abfe01151270b5
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-03 12:07:41 +0000

    Wed, Jul 3, 2024, 9:07 AM -03:00

---
+++ set3/taqueria/taqueria.py
@@ -0,0 +1,33 @@
+foods = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def validation(prompt):
+    while True:
+        try:
+            product = input(prompt).title()
+            if foods:
+                print(f'Total: ${foods[product]:.2f}')
+                pass
+        except ValueError:
+            break
+        except EOFError:
+            break
+        except KeyError:
+            break
+
+
+def main():
+    validation('Item: ')
+
+
+if __name__ == "__main__":
+   main()
