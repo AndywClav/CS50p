bb1a403fe4152e12f37f052fcf6db607e8e880e1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-03 12:15:17 +0000

    Wed, Jul 3, 2024, 9:15 AM -03:00

--- set3/taqueria/taqueria.py
+++ set3/taqueria/taqueria.py
@@ -1,13 +1,13 @@
 foods = {
     "Baja Taco": 4.25,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
+    "Burrito": 27.00,
+    "Bowl": 27.00,
+    "Nachos": 27.00,
     "Quesadilla": 8.50,
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "Taco": 6.00,
+    "Tortilla Salad": 14.00
 }
 
 def validation(prompt):
