b80930f79dc8afbd258c1777aa6373bba55ac8f1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-05 12:59:07 +0000

    Fri, Jul 5, 2024, 9:59 AM -03:00

--- set3/grocery/grocery.py
+++ set3/grocery/grocery.py
@@ -1,18 +1,3 @@
-
-def items():
-    items = {}
-    try:
-        while True:
-            item = input("").strip().lower()
-            items[item] = 0
-            if item in items:
-                items[item] += 1
-                print(f"{items[item]} {items.keys()}")
-    except:
-        return print(items)
-
-items()
-
 def items():
     items = {}
     try:
