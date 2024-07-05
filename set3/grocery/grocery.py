cfea409dc1660bf0722da974c1cd23a992b3cb39
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-05 12:43:52 +0000

    Fri, Jul 5, 2024, 9:43 AM -03:00

--- set3/grocery/grocery.py
+++ set3/grocery/grocery.py
@@ -1,11 +1,15 @@
-items = {}
+
 def items():
-    while True:
-        item = input("").strip().lower()
-        contador = 0
-        items[item] = contador
-        if item in items:
-            items[item] = contador + 1
+    items = {}
+    try:
+        while True:
+            item = input("").strip().lower()
+            contador = 0
+            items[item] = contador
+            if item in items:
+                items[item] = contador + 1
+    except KeyboardInterrupt:
+        return print(f"{items[item]} {items.keys()}")
 
 
-print(f"{items[item]} {items.keys()}")
+items()
