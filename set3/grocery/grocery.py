f779242ff49ed9486d5cd6f2bc06a27203de7044
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-05 12:41:24 +0000

    Fri, Jul 5, 2024, 9:41 AM -03:00

--- set3/grocery/grocery.py
+++ set3/grocery/grocery.py
@@ -1,10 +1,11 @@
 items = {}
-while True:
-    item = input("").strip().lower()
-    contador = 0
-    items[item] = contador
-    if item in items:
-        items[item] = contador + 1
+def items():
+    while True:
+        item = input("").strip().lower()
+        contador = 0
+        items[item] = contador
+        if item in items:
+            items[item] = contador + 1
 
 
 print(f"{items[item]} {items.keys()}")
