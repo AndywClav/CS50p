c77873f96ab62f310591b4209b8e119d44acb3c5
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-14 00:18:03 +0000

    Sat, Jul 13, 2024, 9:18 PM -03:00

--- set3/outdated/outdated.py
+++ set3/outdated/outdated.py
@@ -13,22 +13,9 @@ mouths = [
     "December"
 ]
 
-
-# date = input().strip().title()
-# convert_slash = date.split("/")
-# convert_space = date.split(" ")
-# if convert_slash:
-#     print(f"{date} with slash")
-# elif convert_space:
-#     print(f"{date} with space")
-
 date = input().strip().title()
 
-# Check if the input contains slashes
 if "/" in date:
     print(f"{date} with slash")
-# Check if the input contains spaces
 elif " " in date:
     print(f"{date} with space")
-else:
-    print(f"{date} has no slashes or spaces")
