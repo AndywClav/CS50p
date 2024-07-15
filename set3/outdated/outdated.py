13ef5742900c0192d9cf838b229ad06f38fa0e74
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-15 13:01:41 +0000

    Mon, Jul 15, 2024, 10:01 AM -03:00

--- set3/outdated/outdated.py
+++ set3/outdated/outdated.py
@@ -25,18 +25,29 @@ while True:
                 if date.startswith(mouth) and mouth in mouths:
                     match mouth:
                          case "January":
+                              print(f"{int(year)}-01-{int(day):02d}")
                          case "February":
-                         case "March",
-                         case "April",
-                         case "May",
-                         case "June",
-                         case "July",
-                         case "August",
-                         case "September",
-                         case "October",
-                         case "November",
-                         case "December"
-                    print(f"{int(year)}-{(mouth)}-{int(day):02d}")
+                              print(f"{int(year)}-02-{int(day):02d}")
+                         case "March":
+                              print(f"{int(year)}-03-{int(day):02d}")
+                         case "April":
+                              print(f"{int(year)}-04-{int(day):02d}")
+                         case "May":
+                              print(f"{int(year)}-05-{int(day):02d}")
+                         case "June":
+                              print(f"{int(year)}-06-{int(day):02d}")
+                         case "July":
+                              print(f"{int(year)}-07-{int(day):02d}")
+                         case "August":
+                              print(f"{int(year)}-08-{int(day):02d}")
+                         case "September":
+                              print(f"{int(year)}-09-{int(day):02d}")
+                         case "October":
+                              print(f"{int(year)}-10-{int(day):02d}")
+                         case "November":
+                              print(f"{int(year)}-11-{int(day):02d}")
+                         case "December":
+                              print(f"{int(year)}-12-{int(day):02d}")
                     break
     except:
         break
