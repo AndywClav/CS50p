f5419ceeb719daf79839f330d6deb4bbfb2aa8b1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-15 12:32:18 +0000

    Mon, Jul 15, 2024, 9:32 AM -03:00

--- set3/outdated/outdated.py
+++ set3/outdated/outdated.py
@@ -20,7 +20,7 @@ while True:
             day, mouth, year = date.split("/")
             print(f"{int(year)}-{int(mouth):02d}-{int(day):02d}")
             break
-        elif date in mouths:
+        elif " " in mouths:
                 mouth, day, year = date.split(" ")
                 print(mouth)
     except:
