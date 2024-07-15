1e8e9b1902aef953c32cc134034d4e5ed32acf83
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-15 12:48:55 +0000

    Mon, Jul 15, 2024, 9:48 AM -03:00

--- set3/outdated/outdated.py
+++ set3/outdated/outdated.py
@@ -18,12 +18,14 @@ while True:
         date = input("Dates: ").title()
         if "/" in date:
             day, mouth, year = date.split("/")
-            print(f"{int(year)}-{int(day):02d}-{int(mouth):02d}")
-            break
+            if day <= 31 or mouth <= 12:
+                print(f"{int(year)}-{int(day):02d}-{int(mouth):02d}")
+                break
         elif " " in date:
                 mouth, day, year = date.split(" ")
                 if date.startswith(mouth) and mouth in mouths:
-                    print(f"{int(year)}-{(mouth)}-{int(day):02d}")
-                    break
+                    if day <= 31 or mouth <= 12:
+                        print(f"{int(year)}-{(mouth)}-{int(day):02d}")
+                        break
     except:
         break
