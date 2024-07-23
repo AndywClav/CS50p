61045502f9ad3bd078c8dba314629f88cc50c5f1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:41:16 +0000

    Tue, Jul 23, 2024, 10:41 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -4,9 +4,10 @@ messages = input("Input: ")
 count = 0
 for message in messages:
     if ":" in messages:
+        count += count
         match count:
             case 2:
-                if 
+                if messages == " ":
                     part1, part2 = messages.split(" ")
                     part2 = part2.strip()
                     icon = emoji.emojize(part2)
