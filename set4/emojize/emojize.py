cb46e4c122b4e96e36160d823975890720ebe00d
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:59:44 +0000

    Tue, Jul 23, 2024, 10:59 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -4,8 +4,8 @@ def main():
     try:
         messages = input("Input: ")
         print(emoji.emojize(messages, language='alias'))
-    except OSError as err:
-        print(err)
+    except:
+        print("Your error is")
 
 if __name__ == "__main__":
     main()
