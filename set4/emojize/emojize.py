4410172b691f8c14bb2addb7f763ef9f986d5853
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:14:48 +0000

    Tue, Jul 23, 2024, 10:14 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -2,8 +2,9 @@ import emoji
 
 message = input("Input: ")
 if ":" in message:
-    messa, icon = message.split(" ")
-    print(icon)
-    print(emoji.emojize(icon))
+    #messa, icon = message.split(" ")
+    #print(icon)
+    icon = emoji.emojize(message)
+    print(f"Hola {icon}")
 else:
     print(message)
