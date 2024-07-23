d6a0a061e107602bfdd64a2b526f3ea93b9a4829
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:03:54 +0000

    Tue, Jul 23, 2024, 10:03 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -1,15 +1,3 @@
 import emoji
 
-def emoji(emj):
-    return emoji.emojize(emj)
-
-def inp():
-    message = input("Input: ")
-    if ":" in message:
-        emj = message.split(":")
-        emoj = emoji(emj)
-        print(f"Output {emoj:thumbsup:}")
-    else:
-        print(message)
-
-inp()
+print(emoji.emojize('Python is :thumbs_up:'))
