286b41ee2d5d1f03946fb397a0212a5edb20d9bb
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:30:04 +0000

    Tue, Jul 23, 2024, 10:30 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -3,7 +3,7 @@ import emoji
 messages = input("Input: ")
 if ":" in messages:
     part1, part2 = messages.split(" ")
-    part2 = part2.strip()hello, :earth_africa:
+    part2 = part2.strip()
     icon = emoji.emojize(part2)
     print(f"{part1} {icon}")
 else:
