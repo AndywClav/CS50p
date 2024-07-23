9a3e801f663aaa8f2d5aca0d4f1977f7359b2e5e
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:12:21 +0000

    Tue, Jul 23, 2024, 10:12 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -4,6 +4,6 @@ message = input("Input: ")
 if ":" in message:
     messa, icon = message.split(" ")
     print(icon)
-    print(emoji.emojize(messa, icon))
+    print(emoji.emojize(icon))
 else:
     print(message)
