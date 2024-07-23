fb6f0f3560197d88bba6dd041088c2d2d25893ef
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 14:12:48 +0000

    Tue, Jul 23, 2024, 11:12 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -1,18 +1,17 @@
 import emoji
 
-def get_emoji():
+def get_emoji(msg):
     """
     This is an Fuction for print Emoji
     """
     try:
-        messages = input("Input: ")
-        print(emoji.emojize(messages, language='alias'))
+        print(emoji.emojize(msg, language='alias'))
     except:
         print(f"You have an Error \nVerified your Message")
 
 
 def main():
-    get_emoji()
+    get_emoji(input("Input: "))
 
 
 if __name__ == "__main__":
