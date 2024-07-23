075087b5815d338f33fd2c9d8ad750029d9d6789
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 12:48:12 +0000

    Tue, Jul 23, 2024, 9:48 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -1,7 +1,5 @@
-import sys
 import emoji
 
-try:
-    print("hello, my name is", emoji.emojize(sys.argv[1]))
-except error as er:
-    print(er)
+mesagge = input("Input: ")
+
+print(f"Output {message}")
