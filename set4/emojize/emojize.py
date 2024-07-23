33551d880b9494f413276e913f5bdc30af378f0c
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-23 13:57:14 +0000

    Tue, Jul 23, 2024, 10:57 AM -03:00

--- set4/emojize/emojize.py
+++ set4/emojize/emojize.py
@@ -1,4 +1,11 @@
 import emoji
 
-messages = input("Input: ")
-print(emoji.emojize(messages, language='alias'))
+def main():
+    try:
+        messages = input("Input: ")
+        print(emoji.emojize(messages, language='alias'))
+    except OSError as err:
+        print(err)
+
+if __name__ == "__main__":
+    main()
