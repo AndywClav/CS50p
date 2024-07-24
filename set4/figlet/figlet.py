f49e6216d215d0bf951694202da0efb275022a4d
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-24 12:35:11 +0000

    Wed, Jul 24, 2024, 9:35 AM -03:00

--- set4/figlet/figlet.py
+++ set4/figlet/figlet.py
@@ -1,14 +1,17 @@
 from pyfiglet import Figlet
 import sys
 
-if len(sys.argv):
-    for arg in sys.argv:
-        if arg == "-f" or arg == "--font":
-            f = Figlet(sys.argv[2])
-        else:
-            f = Figlet()
+def text():
+    if len(sys.argv):
+        for arg in sys.argv:
+            if arg == "-f" or arg == "--font":
+                f = Figlet(sys.argv[2])
+            else:
+                f = Figlet()
 
-    message = input("Input: ")
+def main():
+
+message = input("Input: ")
 
 
 print(f.renderText(message))
