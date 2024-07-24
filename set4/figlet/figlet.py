8225001e765f31841e62d45a1609bdf063c1a8cd
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-24 13:05:05 +0000

    Wed, Jul 24, 2024, 10:05 AM -03:00

--- set4/figlet/figlet.py
+++ set4/figlet/figlet.py
@@ -7,21 +7,22 @@ def font_text():
             if len(sys.argv) > 2:
                 return Figlet(font=sys.argv[2])
             else:
+                print("Error: Font name not provided.")
                 sys.exit(1)
         else:
+            print("Error: Invalid argument.")
             sys.exit(1)
     return Figlet()
-
 
 def main():
     try:
         f = font_text()
         message = input("Input: ")
         print(f.renderText(message))
-    except:
+    except Exception as e:
+        print(f"Error: {e}")
         f = Figlet(font='pawp')
         print(f.renderText('ERROR'))
-
 
 if __name__ == "__main__":
     main()
