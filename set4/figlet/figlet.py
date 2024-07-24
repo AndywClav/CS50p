4f3afc7edb956416f1914376347f8ac2599b4ffa
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-24 12:47:07 +0000

    Wed, Jul 24, 2024, 9:47 AM -03:00

--- set4/figlet/figlet.py
+++ set4/figlet/figlet.py
@@ -3,16 +3,14 @@ import sys
 
 def font_text():
     try:
-        if len(sys.argv) > 1:
-            for i, arg in enumerate(sys.argv):
+        if len(sys.argv):
+            for arg in sys.argv:
                 if arg == "-f" or arg == "--font":
-                    if i + 1 < len(sys.argv):
-                        font_name = sys.argv[i + 1]
-                        return Figlet(font=font_name)
-        return Figlet()
-    except Exception as e:
-        print(f"ERROR: {e}")
-        return Figlet(font="doh")
+                    return Figlet(sys.argv[2])
+                else:
+                    return holaFiglet()
+    except:
+        print(f = pyfiglet.figlet_format("ERROR", font="doh"))
 
 
 def main():
