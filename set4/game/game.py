698785b9318e8e57d7b2d337e80ce2ad6ea1ce4a
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-25 15:20:14 +0000

    Thu, Jul 25, 2024, 12:20 PM -03:00

--- set4/game/game.py
+++ set4/game/game.py
@@ -2,18 +2,20 @@
 while True:
     try:
         integre = int(input("Level: "))
-        if integre:
-            while True:
-                try:
-                    game = int(input("Guess: "))
-                    if game == 5:
-                        print("Just right! ")
-                        break
-                except KeyboardInterrupt:
-                    break
-                except:
-                    pass
+        break
     except KeyboardInterrupt:
         break
     except:
         pass
+
+if integre:
+    while True:
+        try:
+            game = int(input("Guess: "))
+            if game == 5:
+                print("Just right! ")
+                break
+        except KeyboardInterrupt:
+            break
+        except:
+            pass
