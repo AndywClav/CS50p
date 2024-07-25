0fcdd3b661b505f3f6c08334ee44b27f8b9c33d7
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-25 14:43:05 +0000

    Thu, Jul 25, 2024, 11:43 AM -03:00

--- set4/adieu/adieu.py
+++ set4/adieu/adieu.py
@@ -4,10 +4,13 @@ p = inflect.engine()
 names = []
 
 while True:
-    message = input("Name: ")
-    if "" == message:
-         break
-    names.append(message)
+    try:
+        message = input("Name: ")
+        if message == "":
+            break
+        names.append(message)
+    except EOFError:
+        break
 
 if names:
     print(f"Adieu, adieu, to {p.join(names)}")
