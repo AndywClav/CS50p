1ec23d8722f532953260bec8558a859ad92f7f9f
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-26 12:31:43 +0000

    Fri, Jul 26, 2024, 9:31 AM -03:00

--- set4/game/game.py
+++ set4/game/game.py
@@ -1,29 +1,36 @@
-# integre = None
-
-# while True:
-#     try:
-#         integre = int(input("Level: "))
-#         if integre > 0:
-#             break
-#     except KeyboardInterrupt:
-#         print("\nProgram exited.")
-#         break
-#     except:
-#         pass
-# if integre:
-#     while True:
-#         try:
-#             game = int(input("Guess: "))
-#             if game == 5: # Put the variable where goint tu random number
-#                 print("Just right! ")
-#                 break
-#         except KeyboardInterrupt:
-#             print("\nProgram exited.")
-#             break
-#         except:
-#             pass
 import random
 
-num = random.randrange(2)
+integre = None
 
-print(num)
+while True:
+    try:
+        integre = int(input("Level: "))
+        if integre > 0:
+            break
+    except KeyboardInterrupt:
+        print("\nProgram exited.")
+        break
+    except:
+        pass
+
+
+if integre:
+    num_random = random.randrange(0, integre)
+    print(num_random + 1, integre)
+    while True:
+        try:
+            game = int(input("Guess: "))
+            if game < 5:
+                print("Too small!")
+            elif game > 5:
+                print("Too large!")
+            elif game == 5: # Put the variable where goint tu random number
+                print("Just right!")
+                break
+        except KeyboardInterrupt:
+            print("\nProgram exited.")
+            break
+        except:
+            pass
+
+
