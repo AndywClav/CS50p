dfb4c23da67010ac32157caa05d704ca651e6ce5
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-29 12:10:40 +0000

    Mon, Jul 29, 2024, 9:10 AM -03:00

---
+++ set4/professor/professor.py
@@ -0,0 +1,43 @@
+import random
+
+
+def main():
+    ...
+
+
+def get_level():
+    while True:
+        try:
+            integre = int(input("Level: "))
+            if integre > 0:
+                return integre
+        except KeyboardInterrupt:
+            print("\nProgram exited.")
+            return None
+        except:
+            pass
+
+
+def generate_integer(level):
+    if level:
+        num_random = random.randint(1, level)
+
+        while True:
+            try:
+                guess = int(input("Guess: "))
+                if guess < num_random:
+                    print("Too small!")
+                elif guess > num_random:
+                    print("Too large!")
+                elif guess == num_random:
+                    print("Just right!")
+                    break
+            except KeyboardInterrupt:
+                print("\nProgram exited.")
+                break
+            except:
+                pass
+
+
+if __name__ == "__main__":
+    main()
