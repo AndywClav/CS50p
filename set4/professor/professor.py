fd99a5aeae7d46538d77d297a4b55aa654fe34b6
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-07-29 13:02:35 +0000

    Mon, Jul 29, 2024, 10:02 AM -03:00

--- set4/professor/professor.py
+++ set4/professor/professor.py
@@ -1,74 +1,74 @@
-import random
+# import random
 
 
-def main():
-    level = get_level()
-    generate_integer(level)
+# def main():
+#     level = get_level()
+#     generate_integer(level)
 
 
-def get_level():
-    while True:
-        try:
-            integre = int(input("Level: "))
-            if integre > 0 and integre < 4:
-                return integre
-        except KeyboardInterrupt:
-            print("\nProgram exited.")
-            return None
-        except:
-            pass
+# def get_level():
+#     while True:
+#         try:
+#             integre = int(input("Level: "))
+#             if integre > 0 and integre < 4:
+#                 return integre
+#         except KeyboardInterrupt:
+#             print("\nProgram exited.")
+#             return None
+#         except:
+#             pass
 
 
-def sum(x, y):
-    return x + y
+# def sum(x, y):
+#     return x + y
 
 
-def validation(x, y):
-    ok = True
-    while ok:
-        try:
-            answer = int(input(f'{x} + {y} = '))
-            value = sum(x, y)
-            if value == answer:
-                score += 1
-                ok = False
-            else:
-                raise ValueError
-        except ValueError:
-            print("EEE")
-        except KeyboardInterrupt:
-            return score
+# def validation(x, y):
+#     ok = True
+#     while ok:
+#         try:
+#             answer = int(input(f'{x} + {y} = '))
+#             value = sum(x, y)
+#             if value == answer:
+#                 score += 1
+#                 ok = False
+#             else:
+#                 raise ValueError
+#         except ValueError:
+#             print("EEE")
+#         except KeyboardInterrupt:
+#             return score
 
 
-def generate_integer(level):
-    if level:
-        score = 0
-        while True:
-            try:
-                match level:
-                    case 1:
-                        num_x_random = random.randint(0, 9)
-                        num_y_random = random.randint(0, 9)
-                        result = validation(num_x_random, num_y_random)
-                    case 2:
-                        num_x_random = random.randint(10, 99)
-                        num_y_random = random.randint(10, 99)
-                        result = validation(num_x_random, num_y_random)
-                    case 3:
-                        num_x_random = random.randint(100, 999)
-                        num_y_random = random.randint(100, 999)
-                        result = validation(num_x_random, num_y_random)
-            except KeyboardInterrupt:
-                score = result
-                print(f"\nScore: {score}")
-                print("Program exited.")
-                break
-            except:
-                pass
+# def generate_integer(level):
+#     if level:
+#         score = 0
+#         while True:
+#             try:
+#                 match level:
+#                     case 1:
+#                         num_x_random = random.randint(0, 9)
+#                         num_y_random = random.randint(0, 9)
+#                         result = validation(num_x_random, num_y_random)
+#                     case 2:
+#                         num_x_random = random.randint(10, 99)
+#                         num_y_random = random.randint(10, 99)
+#                         result = validation(num_x_random, num_y_random)
+#                     case 3:
+#                         num_x_random = random.randint(100, 999)
+#                         num_y_random = random.randint(100, 999)
+#                         result = validation(num_x_random, num_y_random)
+#             except KeyboardInterrupt:
+#                 score = result
+#                 print(f"\nScore: {score}")
+#                 print("Program exited.")
+#                 break
+#             except:
+#                 pass
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 import random
@@ -91,8 +91,10 @@ def get_level():
         except ValueError:
             pass
 
+
 def sum_numbers(x, y):
     return x + y
+
 
 def validation(x, y, score):
     ok = True
@@ -110,6 +112,7 @@ def validation(x, y, score):
         except KeyboardInterrupt:
             return score
     return score
+
 
 def generate_integer(level):
     score = 0
