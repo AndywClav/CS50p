a5b90fcb603160dee9d997513fbde22027dfbc94
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-02 13:32:30 +0000

    Fri, Aug 2, 2024, 10:32 AM -03:00

--- set4/bitcoin/bitcoin.py
+++ set4/bitcoin/bitcoin.py
@@ -2,19 +2,30 @@ import requests
 import json
 import sys
 
-try:
-    r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    if r.status_code == 200:
-        if len(sys.argv) > 1:
-            try:
-                value_btc = sys.argv[2]
-                if float(value_btc):
-                    print(json.dumps(r.json(), indent=2))
-            except:
-                print("Command-line argument is not a number")
-                sys.exit(1)
-        else:
-            print("Missing command-line argument")
-            sys.exit(1)
-except requests.RequestException:
-    print(f'Error in status code your status is {r.status_code}')
+def path(url):
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r
+    except requests.RequestException:
+        print(f'Error in status code your status is {r.status_code}')
+
+
+
+if len(sys.argv) > 1:
+    try:
+        value_btc = sys.argv[2]
+        if float(value_btc):
+            print(json.dumps(r.json(), indent=2))
+    except:
+        print("Command-line argument is not a number")
+        sys.exit(1)
+else:
+    print("Missing command-line argument")
+    sys.exit(1)
+
+def main():
+
+
+if __name__ == '__main__':
+    main():
