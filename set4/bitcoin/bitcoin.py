28512612b8171d7650cb71797957982190bdb0d0
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-02 13:08:18 +0000

    Fri, Aug 2, 2024, 10:08 AM -03:00

--- set4/bitcoin/bitcoin.py
+++ set4/bitcoin/bitcoin.py
@@ -4,7 +4,7 @@ import sys
 
 try:
     r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    if sys.argv[2] > 2:
+    if len(sys.argv) > 2:
         print(json.dumps(r.json(), indent=2))
 except requests.RequestException:
     print(f'Error in status code your status is {r.status_code}')
