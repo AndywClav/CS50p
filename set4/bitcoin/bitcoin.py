c757f2f28a6b541300373dc7987dc2aebd00aab7
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-02 13:18:17 +0000

    Fri, Aug 2, 2024, 10:18 AM -03:00

--- set4/bitcoin/bitcoin.py
+++ set4/bitcoin/bitcoin.py
@@ -4,11 +4,11 @@ import sys
 
 try:
     r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    if len(sys.argv) > 1:
-        try:
+    if r.status_code == 200:
+        if len(sys.argv) > 1:
             print(json.dumps(r.json(), indent=2))
-        except:
+        else:
             print("Missing command-line argument")
-            #sys.exit(1)
+            sys.exit(1)
 except requests.RequestException:
     print(f'Error in status code your status is {r.status_code}')
