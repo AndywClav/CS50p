6739f1fb80e0fe7f13ad01cca8c6b347b5af6fdf
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-06 12:48:31 +0000

    Tue, Aug 6, 2024, 9:48 AM -03:00

--- set5/test_twttr/twttr.py
+++ set5/test_twttr/twttr.py
@@ -1,10 +1,18 @@
-twttr = input("Input: ").strip()
-vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
-out = ""
+def shorten():
+    twttr = input("Input: ").strip()
+    vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+    out = ""
 
-for char in twttr:
-    if char not in vowels:
-        out += char
+    for char in twttr:
+        if char not in vowels:
+            out += char
+
+    return out
 
 
-print(out)
+def main():
+    shorten()
+
+
+if __name__ == "__main__":
+    main()
