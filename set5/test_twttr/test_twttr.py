85e7235291ac30254272c1d734ce43635e5be900
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-06 13:07:33 +0000

    Tue, Aug 6, 2024, 10:07 AM -03:00

--- set5/test_twttr/test_twttr.py
+++ set5/test_twttr/test_twttr.py
@@ -1,13 +1,21 @@
 from twttr import shorten
 
-def test_twttr():
+def test_twttr_vowel():
     assert shorten("mAmAau") == "mm"
+
+
+def test_twttr_number():
     assert shorten("pAPou1") == "pP1"
+
+
+def test_twttr_point():
     assert shorten("joUY.aAN") == "jY.N"
 
 
 def main():
-    test_twttr()
+    test_twttr_vowel()
+    test_twttr_number()
+    test_twttr_point()
 
 
 if __name__ == "__main__":
