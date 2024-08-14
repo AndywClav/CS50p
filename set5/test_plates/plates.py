17e9e4626f90fd358bdfbc96ad09d1ab79739458
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-14 12:27:25 +0000

    Wed, Aug 14, 2024, 9:27 AM -03:00

---
+++ set5/test_plates/plates.py
@@ -0,0 +1,32 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    if not s[:2].isalpha():
+        return False
+
+    if not s.isalnum():
+        return False
+
+    has_number = False
+    for i in range(len(s)):
+        if s[i].isdigit():
+            if not s[i:].isdigit():
+                return False
+            has_number = True
+            if s[i] == '0':
+                return False
+            break
+
+    return True
+
+
+main()
