6149fe2e1df4c928960038c72db81348b8436dd8
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-14 12:33:05 +0000

    Wed, Aug 14, 2024, 9:33 AM -03:00

--- set5/test_plates/test_plates.py
+++ set5/test_plates/test_plates.py
@@ -1,7 +1,12 @@
 from plates import is_valid
 
 def main():
-    ...
+    test_is_valid()
+
 
 def test_is_valid():
-    is_valid()
+    assert is_valid("CS50") == True
+
+
+if __name == "__main__":
+    main()
