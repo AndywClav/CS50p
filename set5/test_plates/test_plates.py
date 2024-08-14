d4595d969910d6d74b3005c15906e9ae94fde6bc
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-14 12:38:28 +0000

    Wed, Aug 14, 2024, 9:38 AM -03:00

--- set5/test_plates/test_plates.py
+++ set5/test_plates/test_plates.py
@@ -6,6 +6,11 @@ def main():
 
 def test_is_valid():
     assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("ECTO88") == True
+    assert is_valid("50") == False
+    assert is_valid("") == True
+    assert is_valid("PI3.14") == False
 
 
 if __name__ == "__main__":
