c222b09e67025804de0986a7019ead4cb18a5bba
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-14 12:40:25 +0000

    Wed, Aug 14, 2024, 9:40 AM -03:00

--- set5/test_plates/test_plates.py
+++ set5/test_plates/test_plates.py
@@ -1,15 +1,19 @@
 from plates import is_valid
 
 def main():
-    test_is_valid()
+    test_is_valid_true()
+    test_is_valid_false()
 
 
-def test_is_valid():
+def test_is_valid_true():
     assert is_valid("CS50") == True
-    assert is_valid("CS05") == False
     assert is_valid("ECTO88") == True
+    assert is_valid("NRVOUS") == True
+
+
+def test_is_valid_false():
+    assert is_valid("CS05") == False
     assert is_valid("50") == False
-    assert is_valid("") == True
     assert is_valid("PI3.14") == False
 
 
