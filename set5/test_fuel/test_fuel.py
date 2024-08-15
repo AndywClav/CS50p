6dd647c16d255d6c6887fbd9356e992c65ddc753
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-15 13:15:59 +0000

    Thu, Aug 15, 2024, 10:15 AM -03:00

--- set5/test_fuel/test_fuel.py
+++ set5/test_fuel/test_fuel.py
@@ -1,23 +1,30 @@
 from fue import convert, gauge
 
 def main():
-    ...
+    test_convert_0():
+    test_convert_25():
+    test_convert_75():
+    test_convert_100():
 
 
 def test_convert_0():
-    assert gauge(0.1) == 'E'
+    percentage = convert("0/1")
+    assert gauge(percentage) == 'E'
 
 
-def test_convert_0():
-    assert gauge() == '25%'
+def test_convert_25():
+    percentage = convert("0/1")
+    assert gauge(percentage) == '25%'
 
 
-def test_convert_0():
-    assert gauge() == '75%'
+def test_convert_75():
+    percentage = convert("0/1")
+    assert gauge(percentage) == '75%'
 
 
-def test_convert_0():
-    assert gauge() == 'F'
+def test_convert_100():
+    percentage = convert("1/1")
+    assert gauge(percentage) == 'F'
 
 
 if __name__ == "__main__":
