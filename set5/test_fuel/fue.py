89f1b38908704cba13f1b870af03a023c17512a1
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-15 12:38:41 +0000

    Thu, Aug 15, 2024, 9:38 AM -03:00

---
+++ set5/test_fuel/fue.py
@@ -0,0 +1,42 @@
+def validation():
+    while True:
+        try:
+            fraction = input("Fraction: ").strip()
+            x, y = fraction.split("/")
+            x = int(x)
+            y = int(y)
+            if x > y or y == 0:
+                continue
+            resul = x / y
+            return resul
+        except ValueError:
+            print(f'ValueError')
+        except ZeroDivisionError:
+            print(f'ZeroDivisionError')
+
+
+def main():
+    fraction = validation()
+    percentage = round(fraction * 100)
+    match percentage:
+        case 0:
+            print('E')
+        case 1:
+            print('E')
+        case 25:
+            print('25%')
+        case 33:
+            print('33%')
+        case 50:
+            print('50%')
+        case 67:
+            print('67%')
+        case 75:
+            print('75%')
+        case 99:
+            print('F')
+        case 100:
+            print('F')
+
+
+main()
