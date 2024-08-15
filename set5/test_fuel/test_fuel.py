d8fef94d9fadfe8901dbd1af7928188f1664d2df
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-15 13:12:19 +0000

    Thu, Aug 15, 2024, 10:12 AM -03:00

--- set5/test_fuel/test_fuel.py
+++ set5/test_fuel/test_fuel.py
@@ -4,5 +4,21 @@ def main():
     ...
 
 
+def test_convert_0():
+    assert gauge() == 'E'
+
+
+def test_convert_0():
+    assert gauge() == '25%'
+
+
+def test_convert_0():
+    assert gauge() == '75%'
+
+
+def test_convert_0():
+    assert gauge() == 'F'
+
+
 if __name__ == "__main__":
     main()
