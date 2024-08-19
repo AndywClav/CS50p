bb89700fd9cbc450e7510c6b028f7f78fae0c034
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-08-19 12:09:53 +0000

    Mon, Aug 19, 2024, 9:09 AM -03:00

--- set5/test_fuel/test_fuel.py
+++ set5/test_fuel/test_fuel.py
@@ -3,39 +3,6 @@ from fue import convert, gauge
 
 def main():
     # test_fuel()
-    test_fuel()
-    test_fuel_25()
-    test_fuel_75()
-    test_fuel_100()
-
-
-# def test_fuel():
-#     convert_fraction = convert("1/0")
-#     if isinstance(convert_fraction, float):
-#             percentage = gauge(convert_fraction)
-#             return print(percentage)
-
-#      assert convert_fraction == "ZeroDivisionError"
-
-
-def test_fuel():
-    percentage = convert("0/1")
-    assert gauge(percentage) == 'E'
-
-
-def test_fuel_25():
-    percentage = convert("1/4")
-    assert gauge(percentage) == 25
-
-
-def test_fuel_75():
-    percentage = convert("9/12")
-    assert gauge(percentage) == 75
-
-
-def test_fuel_100():
-    percentage = convert("1/1")
-    assert gauge(percentage) == '
 
 
 if __name__ == "__main__":
