6a9822ba063932b646320823c3da169d84383256
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-09-04 12:52:55 +0000

    Wed, Sep 4, 2024, 9:52 AM -03:00

--- set6/lines/lines.py
+++ set6/lines/lines.py
@@ -1,10 +1,17 @@
 import sys
 
+EXTEN = [".md", ".py", ".txt"]
+
 if len(sys.argv) > 1:
-    for sys.argv in [".md", ".py", ".txt"]:
-        match sys.argv:
-            case ".md":
-                print("vamos")
+    for sys.argv in len(EXTEN) :
+         if sys.argv == ".md":
+             print("vamos")
 
 
-# Hacer una array para recorrer la cantidad de 
+
+
+
+
+# Hacer una array para recorrer la cantidad de extensiones que permite nuestro programa y validar
+# Hacer una funcion donde valide que el archivo existe y haga la validacion si el mismo esta o no dando un mensaje de error
+# Hacer que cuando este adentro del programa lea todo el archivo y nos diga cuantas lineas de codigo tiene sin contar espacios y lineas
