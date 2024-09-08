5a618c312987abc75ffac0a24bf29951fd85e7de
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-09-08 20:51:14 +0000

    Sun, Sep 8, 2024, 5:51 PM -03:00

--- set6/pizza/pizza.py
+++ set6/pizza/pizza.py
@@ -1,3 +1,27 @@
 import sys
-import csv
 import tabulate
+
+def main():
+    filename = validate_arguments()
+    print(filename)
+
+
+def validate_arguments():
+    if len(sys.argv) < 2:
+        print("Too few command-line arguments")
+        sys.exit
+    elif len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        sys.exit
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        print("Not a Csv file")
+        sys.exit(1)
+
+    return filename
+
+
+if __name__ == "__main__":
+    main()
