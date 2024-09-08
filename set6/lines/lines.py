ec0d361fcbdf9bd564ca80f56c25b146c0f11ac9
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-09-08 20:36:53 +0000

    Sun, Sep 8, 2024, 5:36 PM -03:00

--- set6/lines/lines.py
+++ set6/lines/lines.py
@@ -1,17 +1,42 @@
 import sys
 
-EXTEN = [".md", ".py", ".txt"]
-
-if len(sys.argv) > 1:
-    for i in  sys.argv:
-         if contains():
-             print("vamos")
+def main():
+    filename = validate_arguments()
+    total_lines = count_lines(filename)
+    print(total_lines)
 
 
+def count_lines(filename):
+    try:
+        with open(filename, "r") as file:
+            line_file = file.readlines()
+
+            code_lines = [
+                line for line in line_file
+                if line.strip() != "" and not line.strip().startswith("#")
+            ]
+            return len(code_lines)
+    except FileNotFoundError:
+        print("File does not exist ")
+        sys.exit
 
 
+def validate_arguments():
+    if len(sys.argv) < 2:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".py"):
+        print("Not a Python file")
+        sys.exit(1)
+
+    return filename
 
 
-# Hacer una array para recorrer la cantidad de extensiones que permite nuestro programa y validar
-# Hacer una funcion donde valide que el archivo existe y haga la validacion si el mismo esta o no dando un mensaje de error
-# Hacer que cuando este adentro del programa lea todo el archivo y nos diga cuantas lineas de codigo tiene sin contar espacios y lineas
+if __name__ == "__main__":
+    main()
