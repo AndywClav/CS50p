30a45c89e99b0feacdeabd37a09598cce0ab934a
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-09-11 23:28:38 +0000

    Wed, Sep 11, 2024, 8:28 PM -03:00

---
+++ set6/scourgify/scourgify.py
@@ -0,0 +1,26 @@
+def validate_arguments():
+    if len(sys.argv) < 2:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        print("Not a Csv file")
+        sys.exit(1)
+
+    return filename
+
+
+def file_read(filename):
+    try:
+        with open(filename, "r") as file:
+            render = csv.reader(file)
+            return list(render)
+        
+    except FileNotFoundError:
+        print("File does not exist ")
+        sys.exit(1)
