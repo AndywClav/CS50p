29ddd3061443c2a3e78343d02920a0510a858bfd
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-09-14 00:03:55 +0000

    Fri, Sep 13, 2024, 9:03 PM -03:00

--- set6/shirt/shirt.py
+++ set6/shirt/shirt.py
@@ -1,1 +1,26 @@
-import sys 
+import sys
+
+def main():
+    validate_arguments()
+
+
+def validate_arguments():
+    if len(sys.argv) < 3:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 3:
+        print("Too many command-line arguments")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    new_filename = sys.argv[2]
+
+    if not filename.endswith(".csv") and new_filename:
+        print("Not a Csv file")
+        sys.exit(1)
+
+    return filename, new_filename
+
+
+if __main__ == "__main__":
+    main()
