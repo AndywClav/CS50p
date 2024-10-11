da44b2b83477d1d05bc754a83616e351ae01559d
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-10-11 17:38:49 +0000

    Fri, Oct 11, 2024, 2:38 PM -03:00

--- set6/shirt/shirt.py
+++ set6/shirt/shirt.py
@@ -1,16 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import sys
 import os
 
 def main():
-    filename, new_filaname = validate_arguments()
-    print(filename, new_filaname)
-    shirt = Image.open("shirt.png")
-    photo = Image.open("before1.jpg")
-    size = shirt.size
-    photo.paste(shirt, shirt)
-    print(size)
-    print(photo)
+    filename, new_filename = validate_arguments()
+    combine_photo(filename, new_filename)
 
 
 def validate_arguments():
@@ -24,15 +18,39 @@ def validate_arguments():
     filename = sys.argv[1]
     new_filename = sys.argv[2]
 
-    if not os.path.splitext(filename)[1] or not os.path.splitext(new_filename)[1]:
+    valid_extensions = [".jpg", ".jpeg", ".png"]
+    input_ext = os.path.splitext(filename)[1].lower()
+    output_ext = os.path.splitext(new_filename)[1].lower()
+
+    if input_ext not in valid_extensions or output_ext not in valid_extensions:
         print("Invalid input")
         sys.exit(1)
 
-    if os.path.splitext(filename)[1] != os.path.splitext(new_filename)[1]:
+    if input_ext != output_ext:
         print("Input and output have different extensions")
         sys.exit(1)
 
+    if not os.path.exists(filename):
+        print(f"Input does not exist: {filename}")
+        sys.exit(1)
+
     return filename, new_filename
+
+
+def combine_photo(input_image, output_image):
+    try:
+        with Image.open(input_image) as photo:
+            shirt = Image.open("./shirt.png")
+            photo = ImageOps.fit(photo, size = shirt.size, bleed = 0.0, centering =(0.5, 0.5))
+            photo.paste(shirt, shirt)
+            photo.save(output_image)
+
+    except FileNotFoundError:
+        print(f"Error: '{input_image}' not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
