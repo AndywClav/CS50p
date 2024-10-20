6f37225593e12e5ee152afd6e6b72b1f008a8934
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-10-20 02:44:31 +0000

    Sat, Oct 19, 2024, 11:44 PM -03:00

---
+++ set7/watch/watch.py
@@ -0,0 +1,17 @@
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    """
+    Get url youtube if you put iframe html
+    """
+    if url_yout := re.search() in s:
+        print(url_yout)
+
+
+if __name__ == "__main__":
+    main()
