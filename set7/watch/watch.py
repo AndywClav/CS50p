f138116ab54222b16b341314f89cfd160334eaef
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-10-20 03:45:10 +0000

    Sun, Oct 20, 2024, 12:45 AM -03:00

--- set7/watch/watch.py
+++ set7/watch/watch.py
@@ -11,8 +11,8 @@ def parse(s):
     """
     if url_yout := re.search(r'<iframe.*?src="?(https?://(www\.)?youtube\.com/embed/[^"]+)"?.*?>', s):
         return url_yout.group(1)
-    elif url_yout == re.search(r"^https?://(www\.)?youtube\.com/.*$", s):
-        return url_yout.group(0)
+    elif url_yout == re.search(r"^https?://(www\.)?youtube\.com/.*$", s).group(0):
+        return url_yout
     else:
         return None
 
