596cd28d329cec23ea1ce80702e30921fcf9156b
Andres  Felipe Clavijo Rojas <andresclavijo0822@gmail.com>
2024-10-31 02:00:11 +0000

    Wed, Oct 30, 2024, 11:00 PM -03:00

--- set7/response/response.py
+++ set7/response/response.py
@@ -1,12 +1,12 @@
 from validator_collection import validators
 
 def main():
-    print(validation(input("What's your email address? ")))
+    print(validation_email(input("What's your email address? ")))
 
 
-def validation(em):
+def validation_email(s):
     try:
-        validators.email(em)
+        validators.email(s)
         return f"Valid"
     except ValueError:
         return f"Invalid"
